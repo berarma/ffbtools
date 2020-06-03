4401e35f5dd6da9085d0879886519c01c275c035
Bernat Arlandis <berarma@hotmail.com>
2020-06-03 16:19:48 +0200

    Allow replaying FFB log files

--- src/ffbplay.c
+++ src/ffbplay.c
@@ -36,6 +36,7 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #define print_option(option, text, ...) printf("  %c. " text "\n", option, ##__VA_ARGS__)
 
@@ -165,7 +166,36 @@ void init_effect(struct ff_effect *effect)
             break;
         case FF_RUMBLE:
             effect->u.rumble.strong_magnitude = 0x6000;
-            effect->u.rumble.weak_magnitude = 0x6000;
+            effect->u.rumble.weak_magnitude = 0x2000;
+            break;
+    }
+}
+
+void simple_effect(struct ff_effect *effect)
+{
+    init_effect(effect);
+
+    switch (effect->type) {
+        case FF_CONSTANT:
+            effect->u.constant.level = 0x6000;
+            break;
+        case FF_RAMP:
+            effect->u.ramp.end_level = 0x6000;
+            break;
+        case FF_PERIODIC:
+            effect->u.periodic.period = 1000;
+            effect->u.periodic.magnitude = 0x6000;
+            break;
+        case FF_SPRING:
+        case FF_DAMPER:
+        case FF_FRICTION:
+        case FF_INERTIA:
+            effect->u.condition[0].left_coeff = 0x4000;
+            effect->u.condition[0].right_coeff = 0x4000;
+            break;
+        case FF_RUMBLE:
+            effect->u.rumble.strong_magnitude = 0x6000;
+            effect->u.rumble.weak_magnitude = 0x2000;
             break;
     }
 }
@@ -234,7 +264,7 @@ void menu_effect_parameters(struct ff_effect *effect)
             case FF_FRICTION:
             case FF_INERTIA:
                 print_option('l', "Right saturation: %u", effect->u.condition[0].right_saturation);
-                print_option('m', "Left saturation: %u", effect->u.condition[0].right_saturation);
+                print_option('m', "Left saturation: %u", effect->u.condition[0].left_saturation);
                 print_option('n', "Right coeff: %d", effect->u.condition[0].right_coeff);
                 print_option('o', "Left coeff: %d", effect->u.condition[0].left_coeff);
                 print_option('p', "Deadband: %u", effect->u.condition[0].deadband);
@@ -394,7 +424,7 @@ void menu_upload_effect()
             return;
     }
 
-    init_effect(&effect);
+    simple_effect(&effect);
 
     menu_effect_parameters(&effect);
 
@@ -491,18 +521,278 @@ void main_menu()
     } while (option != 'q');
 }
 
-int main(int argc, char * argv[])
+void new_effect(struct ff_effect *effect, char *params)
 {
-    const char *device_name;
+    char *next_param;
+    char *param;
+    char *key;
+    char *value;
+    int nvalue;
 
-    if (argc == 2) {
-        device_name = argv[1];
-    } else {
-        printf("Syntax: %s <device>\n", argv[0]);
+
+    value = strstr(params, "type:") + 5;
+    if (strstr(value, "CONSTANT") == value) {
+        effect->type = FF_CONSTANT;
+    } else if (strstr(value, "RAMP") == value) {
+        effect->type = FF_RAMP;
+    } else if (strstr(value, "SPRING") == value) {
+        effect->type = FF_SPRING;
+    } else if (strstr(value, "DAMPER") == value) {
+        effect->type = FF_DAMPER;
+    } else if (strstr(value, "FRICTION") == value) {
+        effect->type = FF_FRICTION;
+    } else if (strstr(value, "INERTIA") == value) {
+        effect->type = FF_INERTIA;
+    } else if (strstr(value, "PERIODIC") == value) {
+        effect->type = FF_PERIODIC;
+    } else if (strstr(value, "RUMBLE") == value) {
+        effect->type = FF_RUMBLE;
+    }
+
+    init_effect(effect);
+
+    for(; (param = strtok_r(params, " ", &next_param)); params = NULL) {
+        key = strtok_r(param, ":", &value);
+        if (effect->type == FF_PERIODIC && !strcmp(key, "waveform")) {
+            if (!strcmp(value, "SINE")) {
+                effect->u.periodic.waveform = FF_SINE;
+            } else if (!strcmp(value, "SQUARE")) {
+                effect->u.periodic.waveform = FF_SQUARE;
+            } else if (!strcmp(value, "TRIANGLE")) {
+                effect->u.periodic.waveform = FF_TRIANGLE;
+            } else if (!strcmp(value, "SAW_UP")) {
+                effect->u.periodic.waveform = FF_SAW_UP;
+            } else if (!strcmp(value, "SAW_DOWN")) {
+                effect->u.periodic.waveform = FF_SAW_DOWN;
+            }
+        } else {
+            nvalue = strtol(value, NULL, 0);
+            if (!strcmp(key, "id")) {
+                effect->id = nvalue;
+            } else if (!strcmp(key, "length")) {
+                effect->replay.length = nvalue;
+            } else if (!strcmp(key, "delay")) {
+                effect->replay.delay = nvalue;
+            } else if (!strcmp(key, "dir")) {
+                effect->direction = nvalue;
+            } else {
+                switch (effect->type) {
+                    case FF_CONSTANT:
+                        if (!strcmp(key, "level")) {
+                            effect->u.constant.level = nvalue;
+                        }
+                        break;
+                    case FF_RAMP:
+                        if (!strcmp(key, "start_level")) {
+                            effect->u.ramp.start_level = nvalue;
+                        } else if (!strcmp(key, "end_level")) {
+                            effect->u.ramp.end_level = nvalue;
+                        }
+                        break;
+                    case FF_SPRING:
+                        if (!strcmp(key, "deadband")) {
+                            effect->u.condition[0].deadband = nvalue;
+                        } else if (!strcmp(key, "center")) {
+                            effect->u.condition[0].center = nvalue;
+                        }
+                    case FF_DAMPER:
+                    case FF_FRICTION:
+                    case FF_INERTIA:
+                        if (!strcmp(key, "left_saturation")) {
+                            effect->u.condition[0].left_saturation = nvalue;
+                        } else if (!strcmp(key, "right_saturation")) {
+                            effect->u.condition[0].right_saturation = nvalue;
+                        } else if (!strcmp(key, "left_coeff")) {
+                            effect->u.condition[0].left_coeff = nvalue;
+                        } else if (!strcmp(key, "right_coeff")) {
+                            effect->u.condition[0].right_coeff = nvalue;
+                        }
+                        break;
+                    case FF_RUMBLE:
+                        if (!strcmp(key, "strong_rumble")) {
+                            effect->u.rumble.strong_magnitude = nvalue;
+                        } else if (!strcmp(key, "weak_rumble")) {
+                            effect->u.rumble.weak_magnitude = nvalue;
+                        }
+                        break;
+                    case FF_PERIODIC:
+                        if (!strcmp(key, "period")) {
+                            effect->u.periodic.period = nvalue;
+                        } else if (!strcmp(key, "magnitude")) {
+                            effect->u.periodic.magnitude = nvalue;
+                        } else if (!strcmp(key, "offset")) {
+                            effect->u.periodic.offset = nvalue;
+                        } else if (!strcmp(key, "phase")) {
+                            effect->u.periodic.phase = nvalue;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
+
+void play_file(const char *file_name, int trace_mode)
+{
+    FILE *file = fopen(file_name, "r");
+    struct timespec now;
+    struct timespec start_time;
+    struct ff_effect effect;
+    int delta_time;
+    char line[1024];
+    char *token;
+    char *next_token;
+    char *prefix;
+    char *op;
+    unsigned long time;
+    int first = 1;
+    int id;
+    int count;
+    int ids[255] = {-1};
+    int save_id = -1;
+    int new_id;
+    int return_code;
+
+    if (file == NULL) {
+        printf("Error: %s", strerror(errno));
         exit(1);
     }
 
-    printf("Force feedback test tool.\n\n");
+    printf("Playing %s\n\n", file_name);
+
+    while (fgets(line, sizeof(line), file)) {
+        if (trace_mode) {
+            printf(line);
+        }
+        strtok(line, "\n");
+        token = strtok_r(line, " ", &next_token);
+        time = strtol(token, NULL, 10);
+        if (first) {
+            first = 0;
+            clock_gettime(CLOCK_MONOTONIC, &start_time);
+            start_time.tv_sec -= time / 1000000;
+            start_time.tv_nsec -= (time % 1000000) * 1000;
+        } else {
+            clock_gettime(CLOCK_MONOTONIC, &now);
+            delta_time = (now.tv_sec - start_time.tv_sec) * 1000000 +
+                (now.tv_nsec - start_time.tv_nsec) / 1000;
+            if (time > delta_time) {
+                usleep(time - delta_time);
+            }
+        }
+        if (next_token[0] == '#') {
+            printf("%s\n", next_token);
+            continue;
+        } else {
+            prefix = strtok_r(NULL, " ", &next_token);
+            if (prefix[0] == '<') {
+                if (save_id == -1) {
+                    continue;
+                }
+                return_code = strtol(strtok_r(NULL, " ", &next_token), NULL, 10);
+                if (return_code != 0) {
+                    save_id = -1;
+                    continue;
+                }
+                token = strtok_r(NULL, ":", &next_token);
+                new_id = strtol(next_token, NULL, 10);
+                ids[new_id] = save_id;
+                save_id = -1;
+                continue;
+            }
+            save_id = -1;
+            op = strtok_r(NULL, " ", &next_token);
+            if (!strcmp(op, "QUERY") || !strcmp(op, "SLOTS")) {
+                continue;
+            }
+        }
+        if (!strcmp(op, "GAIN")) {
+            set_gain(strtol(next_token, NULL, 0));
+        } else if (!strcmp(op, "AUTOCENTER")) {
+            set_autocenter(strtol(next_token, NULL, 0));
+        } else if (!strcmp(op, "UPLOAD")) {
+            new_effect(&effect, next_token);
+            if (effect.id == -1) {
+                upload_effect(&effect);
+                save_id = effect.id;
+            } else if (ids[effect.id] != -1) {
+                effect.id = ids[effect.id];
+                upload_effect(&effect);
+            }
+        } else if (!strcmp(op, "PLAY")) {
+            id = strtol(strtok_r(NULL, " ", &next_token), NULL, 0);
+            count = strtol(strtok_r(NULL, " ", &next_token), NULL, 0);
+            if (ids[id] != -1) {
+                play_effect(ids[id], count);
+            }
+        } else if (!strcmp(op, "STOP")) {
+            id = strtol(strtok_r(NULL, " ", &next_token), NULL, 0);
+            if (ids[id] != -1) {
+                play_effect(ids[id], 0);
+            }
+        } else if (!strcmp(op, "REMOVE")) {
+            id = strtol(strtok_r(NULL, " ", &next_token), NULL, 0);
+            remove_effect(id);
+            ids[id] = -1;
+        }
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    const char *device_name;
+    const char *file_name;
+    int interactive_mode = 0;
+    int trace_mode = 0;
+    int c;
+
+    if (argc == 1) {
+        printf("Syntax: %s -d <device> [-i] [-t] [file]\n", argv[0]);
+        exit(1);
+    }
+
+    opterr = 0;
+
+    while ((c = getopt(argc, argv, "d:it")) != -1) {
+        switch (c)
+        {
+            case 'd':
+                device_name = optarg;
+                break;
+            case 'i':
+                interactive_mode = 1;
+                break;
+            case 't':
+                trace_mode = 1;
+                break;
+            case '?':
+                if (optopt == 'd')
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf(stderr,
+                            "Unknown option character `\\x%x'.\n",
+                            optopt);
+                return 1;
+            default:
+                abort();
+        }
+    }
+
+    if (interactive_mode == 0) {
+        if (optind == argc) {
+            fprintf(stderr, "Missing input file for playback.\n");
+            return 1;
+        }
+        if ((optind + 1) < argc) {
+            fprintf(stderr, "Too many arguments.\n");
+            return 1;
+        }
+        file_name = argv[optind];
+    }
+
+    printf("Force feedback playback tool.\n\n");
 
     /* Open event device with write permission */
     device_handle = open(device_name, O_RDWR|O_NONBLOCK);
@@ -514,11 +804,15 @@ int main(int argc, char * argv[])
 
     printf("Using device %s.\n\n", device_name);
 
-    printf("CAUTION: The forces applied might be dangerous.\n");
+    printf("CAUTION: The forces applied might be dangerous.\n\n");
 
     set_gain(0xffff);
 
-    main_menu();
+    if (interactive_mode) {
+        main_menu();
+    } else {
+        play_file(file_name, trace_mode);
+    }
 
     close(device_handle);
 }
